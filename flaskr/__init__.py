85324a15ada5bceed2d57097df3179663ec5113d
ByronZhao <330726651@qq.com>
2020-07-29 15:47:32 +0800

    增加flaskr->__init__.py文件，修改了gitignore文件

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -9,6 +9,15 @@
 # Version   : V1.0.0
 # Info.     : 
 
+import os
+from flask import Flask
+
+def creat_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    return app
+
+
 
 if __name__ == '__main__':
     print("End of the story")
